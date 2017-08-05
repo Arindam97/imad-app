81cf42fc5e411a89ac3b523d2a440a58dcab261e
Arindam97 <30661832+Arindam97@users.noreply.github.com>
2017-08-06 02:57:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.get('ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+var counter= 0;
 app.get('/counter', function(req,res){
     res.send(counter++.toString());
 })
