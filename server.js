76bb70ca287ba3ec2611336f6795070f83cadc8b
Arindam97 <30661832+Arindam97@users.noreply.github.com>
2017-08-06 01:10:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.send("All site two requests are served here.");
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function (req, res) {
