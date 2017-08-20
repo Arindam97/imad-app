d633c5cc8737799f7e85c21651bd648fd3e6d7dd
Arindam97 <30661832+Arindam97@users.noreply.github.com>
2017-08-20 22:16:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,8 +23,10 @@ var name=nameIn.value;
 var submit= document.getElementById('submit_btn');
 submit.onclck= function()
 {
-    var names[]={'name1', 'name2', 'name3'};
+    var names[]=['name1', 'name2', 'name3'];
     var list='';
     for( var i=0; i<name.length; i++)
     list+='<li>'+names[i]+'</li';
+var ul=document.getElementById('namelist');
+ul.innerHTML=list;
 }
