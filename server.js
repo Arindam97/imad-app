78f825e40904c37a1dc05e2406406fa6f4f806ec
Arindam97 <30661832+Arindam97@users.noreply.github.com>
2017-08-22 23:34:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -138,9 +138,6 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
@@ -152,9 +149,6 @@ app.get('/counter', function(req,res){
     res.send(counter.toString());
 });
 
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
