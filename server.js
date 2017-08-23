f9488ca906484e5fcb68332ed70211c8df3a80e4
Arindam97 <30661832+Arindam97@users.noreply.github.com>
2017-08-23 23:29:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -173,7 +173,7 @@ function createTemplate(data){
 
 app.get('/articles/:articleName', function (req, res) {
     var articleName=req.params.articleName;
-    pool.query("SELECT * FROM article WHERE webTitle= '"+parseInt(req.params.articleName)+"'", function(req,res){
+    pool.query("SELECT * FROM article WHERE webTitle= '"+parseInt(req.params.articleName)+"'", function(err, result){
         if(err){
             res.status(500).send(err.toString());
         }else{
