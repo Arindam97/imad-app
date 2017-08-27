f7e321bf48de2ae6862faf00ad446c290d79250c
Arindam97 <30661832+Arindam97@users.noreply.github.com>
2017-08-27 22:51:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,8 +60,8 @@ app.get('/hash/:input', function(req,res){
 });
 
 app.post('/create-user', function(req,res){
-    var username= req.body.username1;
-    var password= red.body.password1;
+    var username= req.body.username;
+    var password= red.body.password;
     var salt= crypto.randomBytes(128).toString('hex');
     var dbString= hash(password, salt) ;
     pool.query('INSERT INTO "user" (username, password) VALUES($1,$2)', [username, dbString], function(err, result){
@@ -77,8 +77,8 @@ app.post('/create-user', function(req,res){
 //login 
 
 app.post('/login', function(req,res){
-    var username= req.body.username;
-    var password= req.body.password;
+    var username= req.body.username1;
+    var password= req.body.password1;
     pool.query('SELECT * FROM "user" WHERE  username= $1', [username], function(err, result){
        if(err){
             res.status(500).send(err.toString());
