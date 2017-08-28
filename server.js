6ae0108a3ac672675053ebf4f54745bcf52a934a
Arindam97 <30661832+Arindam97@users.noreply.github.com>
2017-08-28 22:53:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.post('/login', function(req,res){
        if(err){
             res.status(500).send(err.toString());
         } else{
-            if(results.rows.length===0){
+            if(result.rows.length===0){
                 res.status(404);
             }else{
                 var dbString= result.rows[0].password;
