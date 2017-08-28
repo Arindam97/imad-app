789e6726fddfad02d1037c174a209e4e861b2772
Arindam97 <30661832+Arindam97@users.noreply.github.com>
2017-08-28 23:08:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool= require('pg').Pool;
 var crypto= require('crypto');
 var bodyParser= require('body-parser');
+var session= require('express-session');
 
 var config= {
     user: 'arindammaitra97a81',
@@ -16,6 +17,10 @@ var config= {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'aRandomStringToProtectMyselfFromDangersLikeYou',
+    cookie: {maxage: 1000*60*60*24*30}
+}));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -91,6 +96,7 @@ app.post('/login', function(req,res){
                 var hashedPassword= hash(password, salt);
                 if(hashedPassword === dbString)
                 {
+                     req.session.auth={userId: result.rows[0].id};
                      res.send("USER SUCCESSFULLY VERIFIED");
                 }else{
                     res.status(403)
@@ -102,6 +108,14 @@ app.post('/login', function(req,res){
 
 // end login
 
+app.get('check-login', function(res,req){
+   if(req.session && req.session.auth & req.session.auth.userId){
+       res.send("You are user number "+ req.session.auth.userId.toString());
+   }
+   else{
+       res.send("YOU ARE NOT LOGGED IN");
+   }
+});
 
 function createTemplate(data){
     
