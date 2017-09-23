15e284ab9f98a4d8e7a4feb308faae1a98a69a2b
Arindam97 <30661832+Arindam97@users.noreply.github.com>
2017-09-23 22:31:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool= require('pg').Pool;
-var crypto= require('crypto');
+var Pool= require('pg').Pool;  //database poolling
+var crypto= require('crypto');   // 
 var bodyParser= require('body-parser');
 var session= require('express-session');
 
